b4ee19d5a53fd93d5ae033d4120c4ad9e6231920
HarshadaBodkhe <harshadabodkhe59@gmail.com>
2026-07-18 18:27:58 +0530

    Initial commit

--- src/screens/student_screen.py
+++ src/screens/student_screen.py
@@ -10,7 +10,7 @@ from src.pipelines.face_pipeline import predict_attendance, get_face_embeddings,
 from src.pipelines.voice_pipeline import get_voice_embedding
 from src.database.db import get_all_students, create_student, get_student_subjects, get_student_attendance, unenroll_student_to_subject
 import time
-import cv2
+
 
 from src.components.dialog_enroll import enroll_dialog
 from src.components.subject_card import subject_card
